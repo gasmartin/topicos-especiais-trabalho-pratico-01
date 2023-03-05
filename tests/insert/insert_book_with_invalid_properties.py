d4fd1f9f8de355058c740cfc2d7d78f485141789
Gabriel San Martin <48741663+gasmartin@users.noreply.github.com>
2023-03-05 10:02:26 -0400

    Update insert_book_with_invalid_properties.py

--- tests/insert/insert_book_with_invalid_properties.py
+++ tests/insert/insert_book_with_invalid_properties.py
@@ -1,13 +1,10 @@
-import random
+import time
 
 from page_objects import HomePage
 
 
-def test_consult_book(driver):
+def test_insert_book_with_invalid_properties(driver):
     home_page = HomePage(driver)
-    selected_book = random.choice(home_page.get_registered_books())
-    book_info = home_page.search_book(selected_book['isbn'])
-    assert book_info['name'] == selected_book['name'], 'Retrieved book name is incorrect'
-    assert book_info['author'] == selected_book['author'], 'Retrieved book author is incorrect'
-    assert book_info['num_pages'] == selected_book['num_pages'], 'Retrieved book name is incorrect'
-    assert book_info['num_copies'] == selected_book['num_copies'], 'Retrieved book name is incorrect'
+    home_page.insert_book('xxxxxx', '-1', '-1', 10, 2)
+    time.sleep(3)
+    assert not home_page.is_book_on_table('xxxxxx')
