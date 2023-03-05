4e43a9b16aea469924c9a0d8239eba51c74ad9f9
Gabriel San Martin <48741663+gasmartin@users.noreply.github.com>
2023-03-05 10:00:20 -0400

    Update delete_book.py

--- tests/delete/delete_book.py
+++ tests/delete/delete_book.py
@@ -6,7 +6,8 @@ from page_objects import HomePage
 
 def test_delete_book(driver):
     home_page = HomePage(driver)
-    selected_book = random.choice(home_page.get_registered_books())
-    home_page.delete_book(selected_book['isbn'])
+    isbn = random.choice(home_page.get_registered_books())['isbn']
+    home_page.delete_book(isbn)
     time.sleep(3)
-    assert not home_page.is_book_on_table(selected_book['isbn']), 'Book was not removed from table'
+    deleted = not home_page.is_book_on_table(isbn)
+    assert deleted, 'Book was not removed from table'
